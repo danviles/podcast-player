4fd2900afe8234b49591564cd534dbb00f5c8257
Elvis Nogueiras <danvilapps@gmail.com>
2023-08-29 12:42:55 +0100

    fix: :bug: Fix back button state on search bar

--- src/components/search/SearchBar.tsx
+++ src/components/search/SearchBar.tsx
@@ -1,25 +1,31 @@
 import { useState } from "react";
 import { InputAdornment, TextField } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
-import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import { useGetPodcasts } from "../../hooks/usePodcast";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const SearchBar = () => {
-  const [query, setQuery] = useState('');
+  const location = useLocation();
+  const navigate = useNavigate();
+  const showBackButtom = location.pathname === "/podcast/";
+  const [query, setQuery] = useState("");
   const getPodcasts = useGetPodcasts(query, 0);
 
   const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter') {
-      e.preventDefault(); 
+    if (e.key === "Enter") {
+      e.preventDefault();
       getPodcasts.refetch();
     }
   };
 
   return (
     <div className="flex items-center gap-3">
-      <div className="flex justify-center items-center h-10 w-10 rounded-[15px] bg-[#1A1A1A]">
-        <ArrowBackIosNewIcon className="text-white"/>
-      </div>
+      {showBackButtom && (
+        <div className="flex justify-center items-center h-10 w-10 rounded-[15px] bg-[#1A1A1A] cursor-pointer">
+          <ArrowBackIosNewIcon className="text-white" onClick={() => navigate(-1)} fontSize="large" />
+        </div>
+      )}
       <TextField
         variant="outlined"
         placeholder="Search..."
